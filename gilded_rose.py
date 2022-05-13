06a10022be896d0f619d57e296ff60782109d2d0
Tania Batista <tanbatista@gmail.com>
2022-05-13 08:33:07 -0400

    Add incomplete code

---
+++ gilded_rose.py
@@ -0,0 +1,176 @@
+from typing import List
+import unittest
+
+
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+    def update_quality(self):
+
+        if self.name != "Aged Brie" and self.name != "Backstage passes to a TAFKAL80ETC concert":
+            if self.quality > 0:
+                if self.name != "Sulfuras, Hand of Ragnaros":
+                    self.quality = self.quality - 1
+        else:
+            if self.quality < 50:
+                self.quality = self.quality + 1
+                if self.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if self.sell_in < 11:
+                        if self.quality < 50:
+                            self.quality = self.quality + 1
+                    if self.sell_in < 6:
+                        if self.quality < 50:
+                            self.quality = self.quality + 1
+        if self.name != "Sulfuras, Hand of Ragnaros":
+            self.sell_in = self.sell_in - 1
+
+        if self.sell_in < 0:
+            if self.name != "Aged Brie":
+                if self.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if self.quality > 0:
+                        if self.name != "Sulfuras, Hand of Ragnaros":
+                            self.quality = self.quality - 1
+                else:
+                    self.quality = self.quality - self.quality
+            else:
+                if self.quality < 50:
+                    self.quality = self.quality + 1
+
+
+class AgedBrie(Item):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+        self.name = "Aged Brie"
+
+
+class GildedRose(object):
+
+    # def __init__(self, items):
+    #     self.items = items
+
+    # def refactored_update_item(self):
+    #     pass
+    def __init__(self, items):
+        self.items = List[Item]
+
+    def update(self, items):
+        return [item.update_quality() for item in items]
+
+    def update_quality(self):
+        for item in self.items:
+
+            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality > 0:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
+            else:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                        if item.sell_in < 11:
+                            if item.quality < 50:
+                                item.quality = item.quality + 1
+                        if item.sell_in < 6:
+                            if item.quality < 50:
+                                item.quality = item.quality + 1
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                item.sell_in = item.sell_in - 1
+            if item.sell_in < 0:
+                if item.name != "Aged Brie":
+                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                        if item.quality > 0:
+                            if item.name != "Sulfuras, Hand of Ragnaros":
+                                item.quality = item.quality - 1
+                    else:
+                        item.quality = item.quality - item.quality
+                else:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+
+
+class GildedRoseTest(unittest.TestCase):
+    def test_foo(self):
+        # items = [Item("foo", 0, 0)]
+        items = [aged_brie]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+
+        self.assertEquals("Aged Brie", items[0].name)
+        self.assertEquals(0, items[0].sell_in)
+        self.assertEquals(0, items[0].quality)
+
+    def test_new_item(self):
+        items = [Item("Aged Brie", 5, 5)]
+        old_way = GildedRose(items)
+        old_way.update_quality()
+
+        new_way = Item("Aged Brie", 5, 5)
+        new_way.update_quality()
+
+        aged_brie = AgedBrie("Aged Brie", 5,5)
+        updated_aged_brie.update_quality()
+
+        self.assertEquals(items[0].name, updated_aged_brie.name)
+        self.assertEquals(items[0].sell_in, updated_aged_brie.sell_in)
+        self.assertEquals(items[0].quality, updated_aged_brie.quality)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+aged_brie = Item(
+    name="Aged Brie",
+    sell_in=5,
+    quality=5
+)
+
+
+"""
+https://github.com/emilybache/GildedRose-Refactoring-Kata/blob/main/GildedRoseRequirements.txt
+
+======================================
+Gilded Rose Requirements Specification
+======================================
+
+Hi and welcome to team Gilded Rose. As you know, we are a small inn with a prime location in a
+prominent city ran by a friendly innkeeper named Allison. We also buy and sell only the finest goods.
+Unfortunately, our goods are constantly degrading in quality as they approach their sell by date. We
+have a system in place that updates our inventory for us. It was developed by a no-nonsense type named
+Leeroy, who has moved on to new adventures. Your task is to add the new feature to our system so that
+we can begin selling a new category of items. First an introduction to our system:
+
+	- All items have a SellIn value which denotes the number of days we have to sell the item
+	- All items have a Quality value which denotes how valuable the item is
+	- At the end of each day our system lowers both values for every item
+
+Pretty simple, right? Well this is where it gets interesting:
+
+	- Once the sell by date has passed, Quality degrades twice as fast
+	- The Quality of an item is never negative
+	- "Aged Brie" actually increases in Quality the older it gets
+	- The Quality of an item is never more than 50
+	- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+	- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+	Quality drops to 0 after the concert
+
+We have recently signed a supplier of conjured items. This requires an update to our system:
+
+	- "Conjured" items degrade in Quality twice as fast as normal items
+
+Feel free to make any changes to the UpdateQuality method and add any new code as long as everything
+still works correctly. However, do not alter the Item class or Items property as those belong to the
+goblin in the corner who will insta-rage and one-shot you as he doesn't believe in shared code
+ownership (you can make the UpdateQuality method and Items property static if you like, we'll cover
+for you).
+
+Just for clarification, an item can never have its Quality increase above 50, however "Sulfuras" is a
+legendary item and as such its Quality is 80 and it never alters.
+
+"""
---
+++ robot.py
@@ -0,0 +1,109 @@
+class Robot:
+    def __init__(self):
+        self.direction = "N"
+        self.x = 0
+        self.y = 0
+
+    def get_direction(self, turn):
+        new_directions = ["N", "W", "S", "E"]  # find way to wrap around list
+
+        index = new_directions.index(self.direction)
+        if index == 3:
+            index = 0
+
+        if turn == "L":  #  problem! CANNOT TURN LEFT 5 TIMES
+            index += 1
+
+        if turn == "R":
+            index -= 1
+
+        self.direction = new_directions[index]
+        return self.direction
+
+    def start(self):
+
+        print("Hello! Robot coming online")
+
+        print("""Command the robot with:
+      L - turn left
+      R - turn right
+      M - move forward
+      ? - this message
+      Q - quit """)
+
+        while True:
+
+            instructions = input("> ").upper()
+
+            if instructions in ("L", "R"):
+                self.get_direction(instructions)
+
+            if instructions == "M":
+                if self.direction == "N":
+                    self.y += 1
+
+                if self.direction == "S":
+                    self.y -= 1
+
+                if self.direction == "W":
+                    self.x -= 1
+
+                if self.direction == "E":
+                    self.x += 1
+
+            if instructions == "?":
+                print("""Command the robot with:
+              L - turn left
+              R - turn right
+              M - move forward
+              ? - this message
+              Q - quit """)
+                continue
+
+            if instructions == "Q":
+                print("Robot shutting down")
+                return False
+
+            print(f"Robot at ({self.x}, {self.y}) facing {self.direction}")
+            continue
+
+
+robot = Robot()
+robot.start()
+
+
+"""
+
+Create a command line app that prompts for commands and moves a rover around a 2D plane.
+The robot should point in a direction, turn to face different directions, and move in the direction it is facing.
+
+Demo:
+_________________________
+Hello! Robot coming online.
+Command the robot with:
+  L - turn left TURN
+  R - turn right TURN
+  M - move forward
+  ? - this message
+  Q - quit
+
+> M
+Robot at (0, 1) facing North
+
+> L
+Robot at (0, 1) facing West
+
+> M
+Robot at (-1, 1) facing West
+
+> ?
+Command the robot with:
+  L - turn left
+  R - turn right
+  M - move forward
+  ? - this message
+  Q - quit
+
+> Q
+Robot shutting down.
+"""
