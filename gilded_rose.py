f583b918f82a5c67524d06c467c4c2b3c8856919
Tania Batista <tanbatista@gmail.com>
2022-05-19 09:41:12 -0400

    Revert to original

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,70 +1,57 @@
-from typing import List
-import unittest
+"""
+https://github.com/emilybache/GildedRose-Refactoring-Kata/blob/main/GildedRoseRequirements.txt
 
+======================================
+Gilded Rose Requirements Specification
+======================================
+Hi and welcome to team Gilded Rose.
+As you know, we are a small inn with a prime location in a prominent city ran by a friendly innkeeper named Allison.
+We also buy and sell only the finest goods.
+Unfortunately, our goods are constantly degrading in quality as they approach their sell by date.
+We have a system in place that updates our inventory for us.
+It was developed by a no-nonsense type named Leeroy, who has moved on to new adventures.
+Your task is to add the new feature to our system so that we can begin selling a new category of items.
 
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
+First an introduction to our system:
+- All items have a SellIn value which denotes the number of days we have to sell the item
+- All items have a Quality value which denotes how valuable the item is
+- At the end of each day our system lowers both values for every item
 
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+Pretty simple, right? Well this is where it gets interesting:
+- Once the sell by date has passed, Quality degrades twice as fast
+- The Quality of an item is never negative
+- "Aged Brie" actually increases in Quality the older it gets
+- The Quality of an item is never more than 50
+- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+    Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+    Quality drops to 0 after the concert
 
-    def update_quality(self):
+We have recently signed a supplier of conjured items.
+This requires an update to our system:
+- "Conjured" items degrade in Quality twice as fast as normal items
 
-        if self.name != "Aged Brie" and self.name != "Backstage passes to a TAFKAL80ETC concert":
-            if self.quality > 0:
-                if self.name != "Sulfuras, Hand of Ragnaros":
-                    self.quality = self.quality - 1
-        else:
-            if self.quality < 50:
-                self.quality = self.quality + 1
-                if self.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if self.sell_in < 11:
-                        if self.quality < 50:
-                            self.quality = self.quality + 1
-                    if self.sell_in < 6:
-                        if self.quality < 50:
-                            self.quality = self.quality + 1
-        if self.name != "Sulfuras, Hand of Ragnaros":
-            self.sell_in = self.sell_in - 1
+Feel free to make any changes to the UpdateQuality method and add any new code as long as everything still works correctly.
+However, do not alter the Item class or Items property as those belong to the goblin in the corner
+who will insta-rage and one-shot you as he doesn't believe in shared code ownership
+(you can make the UpdateQuality method and Items property static if you like, we'll cover for you).
 
-        if self.sell_in < 0:
-            if self.name != "Aged Brie":
-                if self.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if self.quality > 0:
-                        if self.name != "Sulfuras, Hand of Ragnaros":
-                            self.quality = self.quality - 1
-                else:
-                    self.quality = self.quality - self.quality
-            else:
-                if self.quality < 50:
-                    self.quality = self.quality + 1
+Just for clarification, an item can never have its Quality increase above 50,
+however "Sulfuras" is a legendary item and as such its Quality is 80 and it never alters.
 
+"""
 
-class AgedBrie(Item):
-    def __init__(self, name, sell_in, quality):
-        super().__init__(name, sell_in, quality)
-        self.name = "Aged Brie"
+"""
+gilded_rose.py
+"""
 
 
 class GildedRose(object):
-
-    # def __init__(self, items):
-    #     self.items = items
-
-    # def refactored_update_item(self):
-    #     pass
     def __init__(self, items):
-        self.items = List[Item]
-
-    def update(self, items):
-        return [item.update_quality() for item in items]
+        self.items = items
 
     def update_quality(self):
         for item in self.items:
-
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
@@ -94,83 +81,62 @@ class GildedRose(object):
                         item.quality = item.quality + 1
 
 
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+"""
+# test_gilded_rose.py
+"""
+
+import unittest
+
+
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        # items = [Item("foo", 0, 0)]
-        items = [aged_brie]
+        items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-
-        self.assertEquals("Aged Brie", items[0].name)
-        self.assertEquals(0, items[0].sell_in)
-        self.assertEquals(0, items[0].quality)
-
-    def test_new_item(self):
-        items = [Item("Aged Brie", 5, 5)]
-        old_way = GildedRose(items)
-        old_way.update_quality()
-
-        new_way = Item("Aged Brie", 5, 5)
-        new_way.update_quality()
-
-        aged_brie = AgedBrie("Aged Brie", 5,5)
-        updated_aged_brie.update_quality()
-
-        self.assertEquals(items[0].name, updated_aged_brie.name)
-        self.assertEquals(items[0].sell_in, updated_aged_brie.sell_in)
-        self.assertEquals(items[0].quality, updated_aged_brie.quality)
+        self.assertEquals("fixme", items[0].name)
 
 
 if __name__ == '__main__':
     unittest.main()
 
-aged_brie = Item(
-    name="Aged Brie",
-    sell_in=5,
-    quality=5
-)
-
-
 """
-https://github.com/emilybache/GildedRose-Refactoring-Kata/blob/main/GildedRoseRequirements.txt
-
-======================================
-Gilded Rose Requirements Specification
-======================================
-
-Hi and welcome to team Gilded Rose. As you know, we are a small inn with a prime location in a
-prominent city ran by a friendly innkeeper named Allison. We also buy and sell only the finest goods.
-Unfortunately, our goods are constantly degrading in quality as they approach their sell by date. We
-have a system in place that updates our inventory for us. It was developed by a no-nonsense type named
-Leeroy, who has moved on to new adventures. Your task is to add the new feature to our system so that
-we can begin selling a new category of items. First an introduction to our system:
-
-	- All items have a SellIn value which denotes the number of days we have to sell the item
-	- All items have a Quality value which denotes how valuable the item is
-	- At the end of each day our system lowers both values for every item
-
-Pretty simple, right? Well this is where it gets interesting:
-
-	- Once the sell by date has passed, Quality degrades twice as fast
-	- The Quality of an item is never negative
-	- "Aged Brie" actually increases in Quality the older it gets
-	- The Quality of an item is never more than 50
-	- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-	- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
-	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
-	Quality drops to 0 after the concert
-
-We have recently signed a supplier of conjured items. This requires an update to our system:
-
-	- "Conjured" items degrade in Quality twice as fast as normal items
-
-Feel free to make any changes to the UpdateQuality method and add any new code as long as everything
-still works correctly. However, do not alter the Item class or Items property as those belong to the
-goblin in the corner who will insta-rage and one-shot you as he doesn't believe in shared code
-ownership (you can make the UpdateQuality method and Items property static if you like, we'll cover
-for you).
-
-Just for clarification, an item can never have its Quality increase above 50, however "Sulfuras" is a
-legendary item and as such its Quality is 80 and it never alters.
-
+texttest_fixture.py
 """
+from __future__ import print_function
+
+if __name__ == "__main__":
+    print("OMGHAI!")
+    items = [
+        Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        Item(name="Aged Brie", sell_in=2, quality=0),
+        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+    ]
+
+    days = 2
+    import sys
+
+    if len(sys.argv) > 1:
+        days = int(sys.argv[1]) + 1
+    for day in range(days):
+        print("-------- day %s --------" % day)
+        print("name, sellIn, quality")
+        for item in items:
+            print(item)
+        print("")
+        GildedRose(items).update_quality()
